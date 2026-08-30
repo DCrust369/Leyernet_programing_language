36d9a9eb7d4b52ea64d683382da86ac438993534
DCrust369 <DCrust@proton.me>
2026-08-30 19:01:00 +0100

    Add files via upload

---
+++ src\compiler/otimizations.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const mimalloc = @import("mimalloc");
+// Apache LICENSE 26/07/2026
+pub fn OptimizeCONSTANT () !void {
+    .var compiler_leyers: i64 = leyernet->compiler->binary; // don't compile for assembly. My language is writed in assembly
+    .var compiler: i32 = mimalloc.allocator();
+    // i optimize
+    var past = nome_archive();
+    var zip = move_archive("for_zip");
+    const zip = 10
+    const archive = 12;
+    const x: u64 = 42;
+    const z: u64 = 43;
+    const zip_archive: i64 = asm  (
+        "mov %[12], %[10]"
+        : [out] "=r"(-> u64)
+        : [in] "r" (22)
+    );
+    return mimalloc.allocator(); 
+}
