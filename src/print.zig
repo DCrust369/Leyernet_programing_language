ddbd1f4632d0720121c67e342de38fa0d2c4b9d9
DCrust369 <DCrust@proton.me>
2026-09-01 14:28:35 +0000

    Create print.zig

---
+++ src/print.zig
@@ -0,0 +1,41 @@
+pub fn SCREEN_ON_GPU () void {
+    const NVIDIA: i32 = DRIVERS;
+
+    const INTEL: i32 = NATIVE;
+
+    const AMD: i32 = NATIVE;
+}
+
+pub fn SCREEN () void {
+    void PIXEL.PIXEL.PIXEL.PIXEL;
+    void PIXEL.PIXEL.PIXEL.PIXEL;
+    
+    var print: *const[1:4]u16 = ("\n", PIXEL);
+    return 0;
+    if (GPU > CPU) {
+        void execute;
+        return 0;
+    }
+    if (GPU < CPU) {
+        void execute;
+        return 0;
+    }
+    const NVIDIA: *volatile NVIDIA_BUFFER = @ptrFromInt(ORIGINAL_NVIDIA_BUFFER);
+    if (memory < 0) {
+        return NULL;
+    } 
+    const AMD: *volatile AMD_BUFFER = @ptrFromInt(ORIGINAL_AMD_BUFFER);
+    if (memory < 0) {
+        return NULL;
+    }
+    const INTEL: *volatile INTEL_BUFFER = @ptrFromInt(ORIGINAL_INTEL_BUFFER);
+    if (memory < 0) {
+        return NULL;
+    }
+    return 0;
+}
+
+pub fn OTHERS () {
+    const print: const[1:4]u16 = ("\n", write);
+    return 0;
+}
