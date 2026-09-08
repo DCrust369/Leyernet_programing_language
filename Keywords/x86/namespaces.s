9216da7b4d21fd052ea5ea58682b7344f4eb86ce
DCrust369 <DCrust@proton.me>
2026-09-08 15:06:04 +0000

    Update namespaces.s

--- Keywords/x86/namespaces.s
+++ Keywords/x86/namespaces.s
@@ -1,57 +1,53 @@
-.section .data
+section .data
 
-.global _start
-
-# namespace
+; namespace
 namespace:
-    .half 100
+    dw 100
 
-# public
-.global pub
+; public
+global pub
 pub:
-    .half 90
+    dw 90
 
-# private
+; private
 pri:
-    .half 90
+    dw 90
 
-# function
-fn:
-    .half 80
+; function
+func:
+    dw 80
 
-# render
+; render
 render:
-    .half 70
+    dw 70
 
-# {}
+; {}
 anonymous:
-    .half 60
+    dw 60
 
 
-.section .text
+section .text
 
-.global _start
+global _start
 
 _start:
 
-    # namespace / pub / pri / func / render / {}
-    la t0, namespace
-    la t1, pub
-    la t2, pri
-    la t3, func
-    la t4, render
-    la t5, anonymous
+    ; namespace / pub / pri / func / render / {}
+    mov bx, namespace
+    mov cx, pub
+    mov dx, func
+    mov si, render
+    mov di, anonymous
 
-    # equivalente conceitual ao:
-    # mov ax, 100, 90, 80, 70, 60
+    ; equivalente conceitual ao:
+    ; mov ax, 100, 90, 80, 70, 60
 
-    lh a0, 0(t0)
-    lh a1, 0(t1)
-    lh a2, 0(t3)
-    lh a3, 0(t4)
-    lh a4, 0(t5)
+    mov ax, [bx]        ; ax = 100
+    mov cx, [cx]        ; cx = 90
+    mov dx, [dx]        ; dx = 80
+    mov si, [si]        ; si = 70
+    mov di, [di]        ; di = 60
 
-    # exit(60)
-    li a0, 60
-    li a7, 93
-    ecall
+    ; exit
+    mov ax, 4C00h
+    int 21h
